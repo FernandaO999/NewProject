acf3b0c81c30f1130d52e099d970ce133e0f744e
FernandaO999 <fernandaolivares999Qgmail.com>
2024-08-25 21:16:27 -0400

    este es mi primer commit

---
+++ MiPrimerArchivo.py
@@ -0,0 +1,39 @@
+
+"""
+Esto es una calculadora
+"""
+def sumar (a,b):
+    return a + b
+
+def restar (a,b):
+    return a - b
+
+def multiplicar (a,b):
+    return a * b
+
+def dividir (a,b):
+    return a / b
+
+print("Bienvenido a la calculadora")
+print("1. Sumar")
+print("2. Restar")
+print("3. Multiplicar")
+print("4. Dividir")
+print("5. Salir")
+
+elección = int(input("Elija una opción:  "))
+num1 = float(input("Ingrese el primer número:  "))
+num2 = float(input("Ingrese el segundo número:  "))
+
+if elección == 1:
+    print(num1, "+",num2,"=", sumar(num1,num2))
+
+elif elección == 2:
+    print(num1, "-",num2,"=", restar(num1,num2))
+elif elección == 3:
+    print(num1, "*",num2,"=", multiplicar(num1,num2))
+elif elección == 4:
+    print(num1, "/",num2,"=", dividir(num1,num2))   
+
+else:
+    print("Opción no valida")
